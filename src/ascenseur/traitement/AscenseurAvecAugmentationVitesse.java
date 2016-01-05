b6e0bddf4aea93e009aba0a989f50371b84f997e
Alexandre <alex83690@live.fr>
2016-01-05 15:44:46 +0100

    Alex

--- src/ascenseur/traitement/AscenseurAvecAugmentationVitesse.java
+++ src/ascenseur/traitement/AscenseurAvecAugmentationVitesse.java
@@ -2,13 +2,13 @@ package ascenseur.traitement;
 
 public class AscenseurAvecAugmentationVitesse extends DecorateurAscenseur {
 
-	public AscenseurAvecAugmentationVitesse(Ascenseur ascenseur) {
-		this.ascenseur = ascenseur;
-	} // Constructor
-	
-	@Override
-	public String getOptionLibelle() {
-		return "Vitesse de déplacement augmentée";
-	} // getOptionLibelle()
+    public AscenseurAvecAugmentationVitesse(Ascenseur ascenseur) {
+        this.ascenseur = ascenseur;
+    } // Constructor
+
+    @Override
+    public String getOptionLibelle() {
+        return "Vitesse de déplacement augmentée";
+    } // getOptionLibelle()
 
 }
