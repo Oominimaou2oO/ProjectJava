876ae37282780ecefe58250fa2ee7f19d2362ed3
Pierre-Antoine Guillaume <pierreantoine.guillaume+github@gmail.com>
2016-01-08 00:13:12 +0100

    package fabrique

--- src/ascenseur/traitement/Fabrique/FabriqueAscenseurVitesse.java
+++ src/ascenseur/traitement/Fabrique/FabriqueAscenseurVitesse.java
@@ -1,11 +1,8 @@
-package ascenseur.traitement.Fabrique;
+package ascenseur.traitement.fabrique;
 
 import ascenseur.traitement.Ascenseur;
 import ascenseur.traitement.AscenseurAvecAugmentationVitesse;
 
-/**
- * Created by Kevin on 07/01/2016.
- */
 public class FabriqueAscenseurVitesse implements IFabrique {
     @Override
     public Ascenseur creer() {
--- src/ascenseur/traitement/Fabrique/IFabrique.java
+++ src/ascenseur/traitement/Fabrique/IFabrique.java
@@ -1,4 +1,4 @@
-package ascenseur.traitement.Fabrique;
+package ascenseur.traitement.fabrique;
 
 import ascenseur.traitement.Ascenseur;
 
@@ -6,6 +6,5 @@ import ascenseur.traitement.Ascenseur;
  * Created by Kevin on 07/01/2016.
  */
 public interface IFabrique {
-
     Ascenseur creer();
 }
