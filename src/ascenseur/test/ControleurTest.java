0b7b6af26a3fe9b644bdd528977ded214443edd6
Kevin HOARAU <Kevin HOARAU>
2016-01-11 15:51:30 +0100

    TEST CONTROLEUR OKOKOKOKOK

--- src/ascenseur/test/ControleurTest.java
+++ src/ascenseur/test/ControleurTest.java
@@ -2,6 +2,8 @@ package ascenseur.test;
 
 import ascenseur.traitement.Constantes;
 import ascenseur.traitement.Controleur;
+import ascenseur.traitement.fabrique.FabriqueAscenseur;
+import ascenseur.traitement.fabrique.IFabrique;
 import org.junit.*;
 import static org.junit.Assert.*;
 
@@ -11,11 +13,11 @@ import static org.junit.Assert.*;
  */
 public class ControleurTest {
 
-    private static Controleur controleur = Controleur.getInstance();
+    private static Controleur controleur;
 
     @BeforeClass
     public static void setUpClass() throws Exception {
-
+        controleur = Controleur.getInstance();
     }
 
     @Test
@@ -28,5 +30,29 @@ public class ControleurTest {
         controleur.creerRequeteExterne(0, 2);
         assertEquals(1,controleur.getRequetesExternes().size());
     }
+
+    @Test
+    public void testAjoutAscenseur() {
+        IFabrique fab = new FabriqueAscenseur();
+        controleur.ajouterAscenseur(fab.creer());
+        assertEquals(1,controleur.getAscenseurs().size());
+    }
+
+    @Test
+    public void testChoisirAscenseur() {
+        IFabrique fab = new FabriqueAscenseur();
+        controleur.ajouterAscenseur(fab.creer());
+        controleur.ajouterAscenseur(fab.creer());
+
+        controleur.creerRequeteExterne(2,3);
+        controleur.creerRequeteExterne(2,3);
+        controleur.creerRequeteExterne(2,3);
+
+        controleur.choisirAscenseur();
+
+        assertEquals(2,controleur.getAscenseurs().get(0).getRequetes().size());
+        assertEquals(0,controleur.getRequetesExternes().size());
+    }
+
 
 }
